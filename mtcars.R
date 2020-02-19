1c717b5dd04c7ce89e2836a95f9950340602749b
habeyo <semharisa@gmail.com>
2020-02-19 23:11:04 +0100

    # this is my first github repository commit commands

---
+++ mtcars.R
@@ -0,0 +1,19 @@
+# HOW TO CREATE and open git using RISTUDIO
+# CREATE GITHUB ACCOUNT THEN IN HTE CLONE DOWNLOAD SECTION COPY THE URL
+# THEN OPEN r-studio from file=> createproject=>vERSIONCONTROL=> AND PUT THE 
+# COPPIED URL EG(https://github.com/habeyo/hats.git)
+# THEN FROM FILE=> NEWFILE=RSCRIPT=> WRITE SOME TIHING AND SAVE IT AS r file
+# then how do we upload this created file to github?
+# tools=>versioncontrol=>commit=> and select the files you want to commit=> and take to
+# the github repository => then write some messeges if ypu want and hit commit
+#a new messege will apear says 2 files changed...etc and close it
+# then hit push  after some procesecess it will end and you close it
+# finaly when you go to your github account 
+
+
+
+library(datasets)
+head(mtcars)
+summary(mtcars)
+str(mtcars)
+plot(mtcars$hp, mtcars$cyl)
